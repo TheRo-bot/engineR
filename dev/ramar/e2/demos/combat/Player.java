b57dfc3763c9c45e6a8432d8832a3dc092d387b6
Robot <the.robot00@gmail.com>
2022-05-14 10:59:38 +0800

    play her? hardly know her.

---
+++ dev/ramar/e2/demos/combat/Player.java
@@ -0,0 +1,34 @@
+package dev.ramar.e2.demos.combat;
+
+
+import dev.ramar.e2.structures.Vec2;
+
+public class Player implements Drawable, Anchor
+{
+    public Player()
+    {
+
+    }
+
+    private Vec2 pos = new Vec2(), 
+                 vel = new Vec2();
+
+
+
+    /* Control Methods
+    --====--------------- 
+    */
+
+
+    
+
+
+    /* Drawing Methods
+    --====---------------
+    */
+
+    public void drawAt(double x, double y, ViewPort vp)
+    {
+
+    }
+}
