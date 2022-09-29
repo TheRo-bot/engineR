c7ee803dbe9e46486e571b9ae4933c08531815f6
Robot <the.robot00@gmail.com>
2022-09-29 23:44:49 +0800

    line zoomies

--- dev/ramar/e2/E2Main.java
+++ dev/ramar/e2/E2Main.java
@@ -61,7 +61,20 @@ public class E2Main
 
         List<Test2> test2s = new ArrayList<>();
         Random rd = new Random();
-
+        for( int ii = 0; ii < 15000; ii++ )
+        {
+            Line t2 = new Line();
+            t2.to = window.mouse.position;
+            t2.getMod()
+                .colour.with(rd.nextDouble(), rd.nextDouble(), rd.nextDouble(), 255)
+                .width.with(1)
+                .offset.with(
+                    rd.nextInt((int)window.getResolutionW()) - window.getResolutionW() * 0.5,
+                    rd.nextInt((int)window.getResolutionH()) - window.getResolutionH() * 0.5
+                )
+            ;
+            window.viewport.layers.addTo(1, t2);
+        }
 
         Test t = new Test()
         {
